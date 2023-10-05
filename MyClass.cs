811d18d1742dfdb04aaecd5e23fb787a5178e882
A7MD <A7MD@A7MD>
2023-10-05 20:28:57 +0300

    Add project files.

---
+++ MyClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGetPackage
+{
+    class MyClass
+    {
+
+        async Task DownloadAsync()
+        {
+
+            HttpClient httpClient = new HttpClient();
+            await httpClient.GetStringAsync("");
+        }
+
+    }
+}
